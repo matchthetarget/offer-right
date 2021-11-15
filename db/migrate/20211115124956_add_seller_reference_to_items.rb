20d6e595c751d2bbde04d317fe421823536241ea
anonymous anonymous <jelani@firstdraft.com>
2021-11-15 12:49:56 +0000

    Add foreign key seller_id reference to items table

---
+++ db/migrate/20211115124956_add_seller_reference_to_items.rb
@@ -0,0 +1,7 @@
+class AddSellerReferenceToItems < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :items, :users, column: :seller_id
+    add_index :items, :seller_id
+    change_column_null :items, :seller_id, false
+  end
+end
