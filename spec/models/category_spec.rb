8c51f6129259b80331275ab2bbdb7becdc858f76
anonymous anonymous <jelani@firstdraft.com>
2021-11-15 12:49:41 +0000

    Validate uniqueness of categories.name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Category, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:name) }
+
     it { should validate_presence_of(:name) }
 
     end
