5ba42e385897fe0c22ecc6403a02416526ff84c7
anonymous anonymous <jelani@firstdraft.com>
2021-11-15 12:50:40 +0000

    Direct association between UserResource and MessageResource

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -8,6 +8,10 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :sent_messages,
+             resource: MessageResource,
+             foreign_key: :sender_id
+
   has_many   :items,
              foreign_key: :seller_id
 
