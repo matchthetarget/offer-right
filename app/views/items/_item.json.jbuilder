d0cf0a700958cb75caff085e58608d5bd56955d8
anonymous anonymous <jelani@firstdraft.com>
2021-11-15 12:49:35 +0000

    Generate items resource with ideas:scaffold

---
+++ app/views/items/_item.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! item, :id, :title, :price, :seller_id, :description, :category_id, :location, :buyer_id, :image, :created_at, :updated_at
+json.url item_url(item, format: :json)
---
+++ db/migrate/20211115124934_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.string :price
+      t.integer :seller_id
+      t.text :description
+      t.integer :category_id
+      t.string :location
+      t.integer :buyer_id
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
